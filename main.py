8c16c3e2232fb1ad9b11c3aaac9ba8e4e8a3d0f5
Shantha Kumar <mailtoshan265@gmail.com>
2023-11-23 00:10:47 +0530

    Add dynamic topic,language, limited news, url and subject.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 from send_mail import send_mail
 
-url = ("https://newsapi.org/v2/everything?q=tesla&from=2023-10-22&sort"
-       "By=publishedAt&apiKey=32c5b4044e104bbea4ab5ef9aad12496")
+topic = "tesla"
+url = (f"https://newsapi.org/v2/everything?q={topic}&from=2023-10-22&sort"
+       "By=publishedAt&apiKey=32c5b4044e104bbea4ab5ef9aad12496&language=en")
 
 # Make request
 request = requests.get(url)
@@ -10,10 +11,11 @@ request = requests.get(url)
 # Get a dictionary
 content = request.json()
 
-body=""
-for article in content['articles']:
+body = "Subject: Today's News" \
+
+for article in content['articles'][:10]:
     if article['title'] is not None:
-        body = body + article['title']+"\n" + article['description'] + 2* "\n"
+        body = body + article['title'] + "\n" + article['description'] + "\n" + article['url'] + 2 * "\n"
 
 body = body.encode("utf-8")
 send_mail(body)
